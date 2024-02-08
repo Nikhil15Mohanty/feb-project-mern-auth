f39ec507253182a8e170c5ac41a8ad26fd183c1a
Nikhil Mohanty <nikhilviki.15@gmail.com>
2024-02-08 22:52:12 +0530

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';        //we cannot use .env file inside the backend so for that reason we need to import it
+dotenv.config()
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('connected to database');
+})
+.catch((err)=>{
+    console.log(err);
+})
+// we use .env file because if we paste the url of mongodb it will show the password
 const app = express();
 
 app.listen(3000,()=>{
