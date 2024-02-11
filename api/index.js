c3d33d374809a0c81f6b15699925d302c9a1839a
Nikhil Mohanty <nikhilviki.15@gmail.com>
2024-02-11 20:43:25 +0530

    create signup route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';        //we cannot use .env file inside the backend so for that reason we need to import it
 import userRoutes from './routes/user.router.js';
+import authRoutes from './routes/auth.router.js';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO)
@@ -14,8 +15,11 @@ mongoose.connect(process.env.MONGO)
 // we use .env file because if we paste the url of mongodb it will show the password
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000,()=>{
     console.log('listening on port 3000!');
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
